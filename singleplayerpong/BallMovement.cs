ab0fa71cc1f2e72e92034f8f307eb93160be3582
John Yeilding <000850543@mohawkcollege.ca>
2024-12-07 16:05:21 -0500

    Created Ball Object and Movement Added a ball object to the scene and made it move in a random direction with a set speed If it collides with a wall it would bounce off of it

---
+++ singleplayerpong/BallMovement.cs
@@ -0,0 +1,52 @@
+using Godot;
+using System;
+
+public partial class BallMovement : CharacterBody2D
+{
+	public const float Speed = 10.0f;
+	Vector2 velocity;
+	int direction;
+	float angle;
+
+	public override void _Ready()
+	{
+		ResetBall();
+	}
+	
+	public void ResetBall()
+	{
+		// Reset the position of the ball to the center of the screen
+		Position = GetViewportRect().Size / 2;
+		
+		// Get random direction on the x axis
+		direction = GD.RandRange(-1, 1);
+		
+		// Fail safe option in case the direction returns 0
+		if(direction == 0)
+		{
+			direction = -1;
+		}
+		
+		// Randomize the angle the ball travels in between values
+		angle = (float)GD.RandRange(-60.0f, 60.0f);
+		
+		// Create a new velocity based on the direction and random angle
+		velocity = new Vector2(direction * Speed, Mathf.Tan(angle));
+		
+	}
+	
+	public override void _PhysicsProcess(double delta)
+	{
+		// Move the ball in a direction (velocity) and check for any collision
+		KinematicCollision2D col = MoveAndCollide(velocity);
+		
+		// When the ball hits a wall, bounce back from it
+		if(col != null)
+		{
+			velocity = velocity.Bounce(col.GetNormal());
+		}
+		
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
