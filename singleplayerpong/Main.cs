94541599de23f0856b98e78aec31aa72f1ff1813
John Yeilding <000850543@mohawkcollege.ca>
2024-12-07 16:55:39 -0500

    Added Reset Score  Attached an event to when the ball overlaps with the area 2D and when it gets called, it resets the ball's position and the score. Fixed bug that if you move down and connect with the ball, you can ride it

--- singleplayerpong/Main.cs
+++ singleplayerpong/Main.cs
@@ -20,4 +20,27 @@ public partial class Main : Node2D
 		//Setting the Label's text to the current score
 		scoreText.SetText("Score: " + score.ToString());
 	}
+	
+	private void ResetScore()
+	{
+		score = 0;
+		
+		//Setting the Label's text to the current score
+		scoreText.SetText("Score: " + score.ToString());
+	}
+	
+	private void OnAreaEntered(Node2D otherNode)
+	{
+		//Getting the node that collided with the Area2D as a BallMovement
+		BallMovement ball = otherNode as BallMovement;
+		
+		//Check to see if what we collided with is a ball
+		//If it is a ball, then we reset the ball and score
+		if(ball != null)
+		{
+			ball.ResetBall();
+			ResetScore();
+			GD.Print("EnteredArea");
+		}
+	}
 }
